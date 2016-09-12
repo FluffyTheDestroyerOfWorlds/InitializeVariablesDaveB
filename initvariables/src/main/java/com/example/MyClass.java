020bad912cde789f07ed65e43c2569f4c5772a5c
FluffyTheDestroyerOfWorlds <theboesens@comcast.net>
2016-09-12 15:19:48 -0500

    Initialize 1 of each type of variable
    Simple program to init vars and send them to stdio

---
+++ initvariables/src/main/java/com/example/MyClass.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class MyClass {
+    public static void main (String[] args){
+        byte i = 1;
+        short s1 = 1000;
+        int iNumber = 550000;
+        long iLong = 999999999;
+        float fNumber = 3.14f;
+        double dNumber = 10.0123565;
+        char cChar = 'x';
+        boolean bIsTrue = false;
+
+    System.out.println("i(Byte) is: " + i + "\n" + "s1(Short) is: " + s1 + "\n" + "iLong(Long) is: " + iLong + "\n" + "fNumber(Float) is: " + fNumber + "\n" + "" +
+    "dNumber(Double) is: " + dNumber + "\n" + "cChar(char) is: " + cChar + "\n" + "bIsTrue(boolean) is " + bIsTrue);
+    }
+}
